7868d63bf1e11e4d5a2f2f1d2c050641516eb492
landonb <landonb@users.noreply.github.com>
2020-03-13 02:51:06 -0500

    Feature: Run projects as groups using MR_INCLUDE environ.

---
+++ lib/skip-grouping.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# vim:tw=0:ts=2:sw=2:et:norl:nospell:ft=sh
+
+# USAGE:
+#
+# In .mrconfig:
+#
+#   [<path>]
+#   skip = mr_exclusive "group"
+#
+# From the CLI:
+#
+#   $ MR_INCLUDE=group mr -d / ls
+#
+# If you set skip on all projects, then this'll return no projects:
+#
+#   $ MR_INCLUDE= mr -d / ls
+#
+# and this'll return all projects still:
+#
+#   $ mr -d / ls
+#
+# To add to more than one group, try:
+#
+#   [<path>]
+#   skip = mr_exclusive "group1" && mr_exclusive "group2"
+#
+# and then any of these will include <path>:
+#
+#   $ MR_INCLUDE=group1 mr -d / ls
+#   $ MR_INCLUDE=group2 mr -d / ls
+#   $ mr -d / ls
+
+mr_exclusive () {
+  ! [ -z ${MR_INCLUDE+x} ] && [ "${MR_INCLUDE}" != "$1" ]
+}
+
