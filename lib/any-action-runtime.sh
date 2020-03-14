4ce58f74d28f63761893fa79db12e4b3e5246611
landonb <landonb@users.noreply.github.com>
2020-03-13 22:50:02 -0500

    Improve: Show simpler, quieter elapsed time value.
    - Squeeze it in as a prefix to the last line (that `mr` prints,
      which reports # ok; # failed; # skipped).
    - Make elapsed time report optional, via minimum threshold value.

--- lib/any-action-runtime.sh
+++ lib/any-action-runtime.sh
@@ -8,32 +8,47 @@ source_deps () {
 reveal_biz_vars () {
   # 2019-10-21: (lb): Because myrepos uses subprocesses, our best bet for
   # maintaining data across all repos is to use temporary files.
-  MR_TMP_RNTIME_FILE='/tmp/home-fries-myrepos.rntime-ieWeich9kaph5eiR'
+  OMR_RUNTIME_TEMPFILE='/tmp/home-fries-myrepos.rntime-ieWeich9kaph5eiR'
+
+  # YOU: Set this to minimum runtime in seconds for elapsed time to be displayed.
+  OMR_RUNTIME_MIN_SECS=${OMR_RUNTIME_MIN_SECS:-0}
+}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+python_prettify_elapsed () {
+  local seconds="${1:-0}"
+  # This spits to stderr if user does not have package installed.
+  /usr/bin/env python -c \
+    "from pedantic_timedelta import PedanticTimedelta; \
+     pdtd = PedanticTimedelta(seconds=${seconds}); \
+     print(pdtd.time_format_scaled(field_width=1, precision=1, abbreviate=2)[0]);" \
+     2> /dev/null
+}
+
+simple_bc_elapsed () {
+  local seconds="${1:-0}"
+  echo "$(echo "scale=1; ${seconds} * 100 / 100" | bc -l) secs."
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
 git_any_command_started () {
-  date +%s.%N > "${MR_TMP_RNTIME_FILE}"
+  date +%s.%N > "${OMR_RUNTIME_TEMPFILE}"
 }
 
 git_any_command_stopped () {
-  local SETUP_TIME_0=$(cat "${MR_TMP_RNTIME_FILE}")
-  [ -z ${SETUP_TIME_0} ] && error "ERROR:" \
+  local setup_time_0=$(cat "${OMR_RUNTIME_TEMPFILE}")
+  [ -z ${setup_time_0} ] && error "ERROR:" \
     "Missing start time! Be sure to call \`git_status_cache_setup\`."
-  local SETUP_TIME_N="$(date +%s.%N)"
-  local time_elapsed=$(\
-    echo "scale=2; ($SETUP_TIME_N - $SETUP_TIME_0) * 100 / 100" | bc -l
-  )
-  # One could only show elapsed time if greater than a specific duration. E.g.,
-  #   # NOTE: Use `bc` to output 0 or 1, and use ``(( ... ))`` so the shell
-  #   #       interprets the result as false or true respectively.
-  #   if (( $(echo "${time_elapsed} > 0.25" | bc -l) )); then ... fi
-  # but the author is fine with this hardcoded. We could make config'able, though.
-  info
-  info "$(attr_bold)$(bg_lime)$(fg_black)Elapsed: ${time_elapsed} secs.$(attr_reset)"
-  info
-  /bin/rm "${MR_TMP_RNTIME_FILE}"
+  local setup_time_n="$(date +%s.%N)"
+  local seconds=$(echo "${setup_time_n} - ${setup_time_0}" | bc -l)
+  if [ $(echo "${seconds} >= ${OMR_RUNTIME_MIN_SECS}" | bc -l) -ne 0 ]; then
+    local time_elapsed="$(python_prettify_elapsed "${seconds}")"
+    [ -z "${time_elapsed}" ] && time_elapsed="$(simple_bc_elapsed "${seconds}")"
+    echo -n "$(attr_emphasis)(${time_elapsed})$(attr_reset) "
+  fi
+  /bin/rm "${OMR_RUNTIME_TEMPFILE}"
 }
 
 git_any_cache_setup () {
