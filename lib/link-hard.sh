3be54ce4f62e4da08d01ae80854ab5ed8316e471
landonb <landonb@users.noreply.github.com>
2021-01-26 15:51:44 -0600

    Feature: New `hard_link` utility.
    - Useful for managing cross-project in-source dependencies,
      i.e., those dependencies you include in a project directly
      so the user does not have to install them themselves (for
      projects where dependencies are not as easy to require a
      user to setup themselves, e.g., simple Bash dependencies).
    
      - The idea is to include a copy of a dependency (e.g., .sh file)
        in a project but to then hard link that dependency on your local
        development machine to its source project's file path, so that
        you don't have to worry about code diverging, and so that when
        you change one file, you change them all, and other repos that
        include the dependency will show up as untidy (e.g., when running
        a my-merge-status command on all your projects) and you'll be
        reminded to update the dependency in all the projects that use
        it.

---
+++ lib/link-hard.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+# vim:tw=0:ts=2:sw=2:et:norl:nospell:ft=bash
+
+source_deps () {
+  :
+}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# Pass in a full path to the canon file ($1)
+# and a partial path to local deps/ file to hard link.
+# WHY: If you package shell or Git dependencies in source code of
+# other projects and don't want to accidentally have different
+# copies of the source diverge; and you only want search results
+# from one copy of the file when grepping across projects.
+link_hard () {
+  # The reference file.
+  local canon_file="$1"
+  # The local project's ($MR_REPO's) reference.
+  local local_file="${MR_REPO}/$2"
+
+  # Use `ls -i` to get the inode, e.g.,:
+  #   $ ls -i ${canon_file}
+  #   55182863 /path/to/file
+
+  file_index_number_or_warn () {
+    local file_path="$1"
+    local file_inode
+
+    if [ ! -f "${file_path}" ]; then
+      >&2 warn "File not found: ${file_path}"
+      return 1
+    fi
+
+    file_inode=$(ls -i "${file_path}" | cut -d' ' -f1 2> /dev/null)
+    if [ $? -ne 0 ] || [ -z "${file_inode}" ]; then
+      >&2 error "No file index for: ${file_path}"
+      return 1
+    fi
+
+    printf '%s' "${file_inode}"
+  }
+
+  if [ -e "${local_file}" ]; then
+    local canon_inode
+    local local_inode
+    canon_inode=$(file_index_number_or_warn "${canon_file}") || return 1
+    local_inode=$(file_index_number_or_warn "${local_file}") || return 1
+
+    # Compare inode values.
+    if [ "${canon_inode}" = "${local_inode}" ]; then
+      # Same inode; already at the desired state.
+      info "Hard link confirmed: $(basename ${local_file})"
+      return 0
+    elif ! diff -q "${canon_file}" "${local_file}" > /dev/null; then
+      # Different inode, and different file contents. Cannot proceed.
+      warn "Refuses to hard link disparate files:"
+      warn "  meld '${canon_file}' '${local_file}' &"
+      return 1
+    fi
+  fi
+
+  # Different inode but nothing diff means we're cleared to clobber.
+  ln -f "${canon_file}" "${local_file}"
+  # info "File hard link created: $(basename ${local_file})"
+  info "Hard link created: $(basename ${local_file})"
+}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+main () {
+  source_deps
+}
+
+main "$@"
+
