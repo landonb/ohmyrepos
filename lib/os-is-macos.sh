94532fd106641384031b575fd34ab4e5a43c4a05
landonb <landonb@users.noreply.github.com>
2023-01-30 12:16:43 -0600

    Feature: Inject new `os_is_macos` function into runtime

---
+++ lib/os-is-macos.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# vim:tw=0:ts=2:sw=2:et:norl:nospell:ft=bash
+# Author: Landon Bouma (landonb &#x40; retrosoft &#x2E; com)
+# Project: https://github.com/landonb/ohmyrepos#😤
+# License: MIT
+
+os_is_macos () {
+  [ "$(uname)" = 'Darwin' ]
+}
+
