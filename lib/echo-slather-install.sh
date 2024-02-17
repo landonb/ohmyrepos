e96b0d373c21de6628580e9e3785796633db3219
Landon Bouma <landonb@users.noreply.github.com>
2024-02-17 01:06:45 -0600

    Feature: New `slatherInstall` action prints copy-pasteable install commands
    - When onboarding a new machine, you can now print a list of all install
      commands, e.g.,:
    
        mr -d / -M slatherInstall
    
      This is preferable over running `mr -d / install`, because if one
      install action fails, if you re-run that command, you'll end up
      reinstalling projects that were successfully installed before
      whatever project failed, and that could take a while.
    
      - Alternatively, you could use `mr --interactive` to debug along
        the way. But running 'install' actions individually is probably
        gonna be easier (and quicker). And you won't "be forced" to
        debug each failed install just to complete the whole operation,
        but can choose to revisit projects that failed at a later time.
    
      - Not that I'm saying any of your 'install' actions should fail,
        but the install actions tend to be more fragile, especially
        when upgrading an OS (and system libraries have changed, etc.).

---
+++ lib/echo-slather-install.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+# vim:tw=0:ts=2:sw=2:et:norl:nospell:ft=bash
+# Author: Landon Bouma (landonb &#x40; retrosoft &#x2E; com)
+# Project: https://github.com/landonb/ohmyrepos#😤
+# License: MIT
+
+# This fcn. used by slather-defaults (macOS-onboarder) to print list
+# of copy-paste OMR install tasks.
+
+echo_slather_install () {
+  local which_os="${1:-os_all}"
+  local dxy_scope="${2:-dxy_all}"
+  local addendum="$3"
+
+  # if ! ${SLATHER_DEFAULTS_ENABLE:-false}; then
+  #   return 0
+  # fi
+
+  # Convert to lowercase.
+  which_os="$(echo "${which_os}" | tr '[:upper:]' '[:lower:]')"
+  dxy_scope="$(echo "${dxy_scope}" | tr '[:upper:]' '[:lower:]')"
+
+  local checkbox="🔳"
+
+  if [ "${which_os}" = "os_linux" ]; then
+    if [ "$(uname)" != 'Linux' ]; then
+      checkbox="❌"
+    fi
+  elif [ "${which_os}" = "os_macos" ]; then
+    if [ "$(uname)" != 'Darmin' ]; then
+      checkbox="❌"
+    fi
+  elif [ "${which_os}" = "os_none" ]; then
+    checkbox="❌"
+  elif [ "${which_os}" != "os_all" ]; then
+    >&2 echo "ERROR: Unknown \`echo_slather_install\` OS target: ${which_os}"
+
+    checkbox="❌"
+  fi
+
+  if [ "${dxy_scope}" = "dxy_pro" ]; then
+    if ${SLATHER_DEFAULTS_LIMITED:-false}; then
+      checkbox="❌"
+    fi
+  elif [ "${dxy_scope}" = "dxy_none" ]; then
+    checkbox="❌"
+  elif [ "${dxy_scope}" != "dxy_all" ]; then
+    >&2 echo "ERROR: Unknown \`echo_slather_install\` env. scope: ${dxy_scope}"
+
+    checkbox="❌"
+  fi
+
+  local app_name="$(basename "${MR_REPO}")"
+
+  local addendum_txt=""
+  if [ -n "${addendum}" ]; then
+    addendum_txt=" (${addendum})"
+  fi
+
+  echo "${checkbox} DepoXy: Install \`${app_name}\` from source${addendum_txt}::
+   mr -d \"${MR_REPO}\" -n install
+"
+}
+
