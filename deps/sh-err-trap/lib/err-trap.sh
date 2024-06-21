b89c4f3ee13aff8e5a9bca1a1ea55d7c256466f6
Landon Bouma <landonb@users.noreply.github.com>
2024-06-21 04:26:05 -0500

    Deps: Update faithfuls <sh-err-trap>
    - Source: sh-err-trap @ ac357e909f66 [n/a]
    
    - Commit generated by:
    
        https://github.com/thegittinsgood/git-update-faithful#⛲
    
      under the guidance of:
    
        https://github.com/DepoXy/depoxy/blob/release/home/.kit/git/ohmyrepos/lib/my-deps-manage-shoilerplate.sh

---
+++ deps/sh-err-trap/lib/err-trap.sh
@@ -0,0 +1,69 @@
+# vim:tw=0:ts=2:sw=2:et:norl:ft=bash
+# Author: Landon Bouma <https://tallybark.com/>
+# Project: https://github.com/DepoXy/sh-err-trap#🪤
+# License: MIT
+
+# Copyright (c) © 2021-2024 Landon Bouma. All Rights Reserved.
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+os_is_macos () {
+  [ "$(uname)" = "Darwin" ]
+}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# Preserve tty flags
+_TTY_FLAGS="$([ -t 0 ] && stty -g)" \
+  || true
+
+clear_traps () {
+  trap - EXIT INT
+}
+
+set_traps () {
+  trap -- trap_exit EXIT
+  trap -- trap_int INT
+}
+
+exit_0 () {
+  clear_traps
+
+  exit 0
+}
+
+exit_1 () {
+  clear_traps
+
+  exit 1
+}
+
+trap_exit () {
+  clear_traps
+
+  # USAGE: Alert on unexpected error path, so you can add happy path.
+  >&2 echo "ALERT: "$(basename -- "$0")" exited abnormally!"
+  >&2 echo "- Hint: Enable \`set -x\` and run again..."
+
+  exit 2
+}
+
+trap_int () {
+  clear_traps
+
+  # Restore tty flags
+  # - If not, `exit` from INT trap might leave terminal in 'silent mode'
+  #   (and nothing is echoed as user types).
+  #   - E.g., if user <Ctrl-C>'s a `read -s` prompt.
+  #   - SAVVY: Pro tip: If this ever happens to you, type `stty sane<CR>`
+  #     to the terminal and hopefully that'll right the sh'ip.
+  # - Note that `stty sane` also works here, but that's more like a reset.
+  #   We use save-load to show that we restored the original tty settings.
+  [ -t 0 ] && stty "${_TTY_FLAGS}" \
+    || true
+
+  exit 3
+}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
