cc953378dafcd2897b0fb2c4b31641707a5d51f6
Landon Bouma <landonb@users.noreply.github.com>
2024-07-01 20:58:52 -0500

    Docs: Cleanup comment

--- lib/skip-grouping.sh
+++ lib/skip-grouping.sh
@@ -70,7 +70,6 @@ mr_exclusive () {
 
     # Check if OMR config uses negated tag.
     # - E.g., `skip = mr_exclusive "!foo"`.
-    local nonnegated="$(echo "!abbddd" | sed 's/^!//')"
     local nonnegated="$(echo "${tag}" | sed 's/^!//')"
     [ "${tag}" != "${nonnegated}" ] \
       && [ "${MR_INCLUDE}" != "${nonnegated}" ] \
