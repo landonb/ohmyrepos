d81a2e94c70c8cb239e588bee4acacef60698409
Landon Bouma <landonb@users.noreply.github.com>
2025-02-23 10:10:03 -0600

    docs: adjust comments

--- lib/sync-travel-remote.sh
+++ lib/sync-travel-remote.sh
@@ -474,9 +474,9 @@ git_travel_verify_mr_action () {
 }
 
 git_travel_cache_setup () {
-  # BWARE/2023-05-01: `mr` leaves MR_ACTION unset on setup and teardown.
+  # BWARE/2023-05-01: Orig. `mr` leaves MR_ACTION unset on setup and teardown.
   # - Author will try to merge this upstream, we'll see.
-  # - In the meantime, just know that every setup and every teardown runs
+  # - In the meantime, just know that every setup and every teardown may run
   #   for every action (so play nice).
   # - CXREF: See longer comment in `git_status_cache_setup`.
   git_travel_verify_mr_action || return 0
