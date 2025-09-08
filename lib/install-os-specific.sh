795af061585093aca986af7b60743c717a0668a9
Landon Bouma <landonb@users.noreply.github.com>
2025-09-08 13:26:17 -0500

    refactor(lint): reformat by shfmt

--- lib/install-os-specific.sh
+++ lib/install-os-specific.sh
@@ -7,7 +7,7 @@
 # This fcn. used by slather-defaults (macOS-onboarder) to print list
 # of copy-paste OMR install tasks.
 
-install_os_specific () {
+install_os_specific() {
   if os_is_macos; then
     mr -d ${MR_REPO} -n installDarwin
   else
