ee5d269de18a07031e4e0094235714a5e9855da0
Landon Bouma <landonb@users.noreply.github.com>
2025-10-26 22:38:00 -0500

    feat(lib): add upgrade_os_specific et al, like install_os_specific

---
+++ lib/upgrade-os-specific.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# vim:tw=0:ts=2:sw=2:et:norl:nospell:ft=bash
+# Author: Landon Bouma <https://tallybark.com/>
+# Project: https://github.com/landonb/ohmyrepos#😤
+# License: MIT
+
+upgrade_os_specific() {
+  if os_is_macos; then
+    mr -d ${MR_REPO} -n upgradeDarwin
+  else
+    mr -d ${MR_REPO} -n upgradeLinux
+  fi
+}
