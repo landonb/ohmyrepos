3d7dedf9da57d01b7fb2cfa5aecc9973d8eaa0e9
Landon Bouma <landonb@users.noreply.github.com>
2025-11-17 01:37:22 -0600

    update(ux): lowercase aci commit messages

--- lib/git-auto-commit.sh
+++ lib/git-auto-commit.sh
@@ -262,7 +262,7 @@ git_auto_commit_path_one() {
     exit 1
   fi
 
-  local msg_prefix="update(aci): Add Favorite: [@$(hostname)]"
+  local msg_prefix="update(aci): add favorite: [@$(hostname)]"
   local commit_msg="${MR_GIT_AUTO_COMMIT_MSG:-${msg_prefix} “$(basename -- "${repo_file}")”}"
 
   local inclT=""
@@ -406,7 +406,7 @@ git_auto_commit_many() {
   fi
 
   if [ ${MR_GIT_AUTO_COMMIT_STAGE_COUNT} -gt 0 ]; then
-    local msg_prefix="update(aci): Add Favorite: [@$(hostname)]"
+    local msg_prefix="update(aci): add favorite: [@$(hostname)]"
     local commit_msg="${MR_GIT_AUTO_COMMIT_MSG:-${msg_prefix} ${MR_GIT_AUTO_COMMIT_FILES_ADDED}.}"
     git_auto_commit_path_one_or_many "${commit_msg}"
   fi
