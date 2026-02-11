1189847ebc62db8e7c52e69e87970444cebf2190
Landon Bouma <landonb@users.noreply.github.com>
2026-02-11 16:37:40 -0600

    docs: remove inaccurate comment (copy-pasta stowaway)

--- lib/install-os-specific.sh
+++ lib/install-os-specific.sh
@@ -4,9 +4,6 @@
 # Project: https://github.com/landonb/ohmyrepos#😤
 # License: MIT
 
-# This fcn. used by slather-defaults (macOS-onboarder) to print list
-# of copy-paste OMR install tasks.
-
 install_os_specific() {
   if os_is_macos; then
     mr -d ${MR_REPO} -n installDarwin "$@"
